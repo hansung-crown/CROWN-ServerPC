b80a1d83ee64fb4ab0029dc9d57af777d23d48ed
hyeyeon0111 <hyeyeonpark7183@gmail.com>
2021-01-28 01:16:21 +0900

    Revert "Initial Commit"
    This reverts commit 5d50564f21cc79efaa606e21eaf5db0d919ea377.

--- Danger.py
+++
@@ -1,6 +0,0 @@
-from Human import Human
-
-class Danger(Human):
-    def __init__(self, name):
-        self.name = name
-        self.groupName = "Danger"
--- Known.py
+++
@@ -1,6 +0,0 @@
-from Human import Human
-
-class Known(Human):
-    def __init__(self, name):
-        self.name = name
-        self.groupName = "Known"
